675d4169872b6f557162fc08be3f444546b96b7c
whitexiong <986247535@qq.com>
2021-09-30 16:20:23 +0800

    create gin framework

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "搭建完成")
+	})
+	r.Run(":8888") // 端口号8888
+}
